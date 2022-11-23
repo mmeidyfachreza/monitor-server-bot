a96b002c4a65ae411a10239f9fdde82f6e6d1b0f
mmeidyfachreza <meidyfachreza@gmail.com>
2022-11-23 21:28:33 +0800

    fix typo

--- src/storage-warning-telegram.sh
+++ src/storage-warning-telegram.sh
@@ -2,8 +2,8 @@
 DISK=$(df / | grep '/')
 NAME=$(echo $DISK | awk '{print $1}')
 CURRENT=$(echo $DISK | awk '{print $5}' | sed 's/%//g')
-THRESHOLD=threshold
+THRESHOLD=thres
 
 if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
-    . /etc/telegram/telegram-notify --error --title "Storage Warning For Server1!" --text "Your $NAME partition available space is cri$
+    . /etc/telegram/telegram-notify --error --title "Storage Warning For Server1!" --text "Your $NAME partition available space is critically low. Used: $CURRENT%"
 fi
